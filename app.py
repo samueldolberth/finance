eb60d975fb04d9d98405f212b4a376953bdd7c3b
samueldolberth <samudolberth@gmail.com>
2026-08-10 21:50:04 -0300

    metodo POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,20 @@ receitas = [
 @app.route('/receitas', methods=['GET'])
 def get_receitas():
     return jsonify(receitas)
+
+@app.route('/receitas', methods=['POST'])
+def adicionar_receita():
+    dados = request.get_json()
+
+    nova_receita = {
+        "id": len(receitas) + 1,
+        "descricao": dados["descricao"],
+        "valor": dados["valor"]
+    }
+
+    receitas.append(nova_receita)
+
+    return jsonify(nova_receita), 201
 
 if __name__ == '__main__':
     app.run(debug=True)

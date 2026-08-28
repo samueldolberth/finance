13b512dc2959c773d6a80fb01a43e8b67599d933
samueldolberth <samudolberth@gmail.com>
2026-08-28 20:29:27 -0300

    bkp

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from routes.home import home_route
+from flask import Flask, request
 
 # inicializaçãp
 app = Flask(__name__)
@@ -8,6 +7,19 @@ receitas = [
     {"id": 1, "nome": ""}
 ]
 
-app.register_blueprint(home_route)
+@app.route("/")
+def index():
+    return "Bem vindo ao FINANCE"
 
-app.run(debug=True)
+@app.route("/sobre")
+def sobre():
+    return "Página Sobre"
+
+@app.route("/novo", methods=["GET", "POST"])
+def novo():
+    if request.method == "POST":
+        return "Dados recebidos (em construção)"
+    return "Formunlário de cadastro (em construção)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
